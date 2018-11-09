1f30956a97314190b389a2af9766a4f69d02cdc4
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-11-09 12:37:05 +0100

    Init some FontAwesome icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import './utils/FontAwesome.init';
 import './App.css';
-
 import Header from './components/Header';
 
 class App extends Component {
---
+++ src/utils/FontAwesome.init.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+	faShoppingCart, faTruck, faRetweet, faClock, faCreditCard, faAddressCard, faClipboardList
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faShoppingCart);
+library.add(faTruck);
+library.add(faRetweet);
+library.add(faClock);
+library.add(faCreditCard);
+library.add(faClipboardList);
