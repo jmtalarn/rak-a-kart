8881b6b5dc64b4fa6a25382d41b59d47cd90b053
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-11-09 15:09:29 +0100

    Rough autocomplete on search

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../../logo.png';
 import './header.css';
+import Select from 'react-select';
+import products from '../../data/products.json';
 
 export default class Header extends Component {
 	render() {
+		const options = products.map(product => (
+			{
+				value: product.id,
+				label: `${product.name} - ${[ ...product.description.split(" ").slice(0, 8), "..." ].join(" ")}`
+			})
+		);
 		return (
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<div className="search">
 					<FontAwesomeIcon className="search-icon" icon="search" />
-					<input className="search-input" type="text" placeholder="Search for some car brands to find our awesome products..." />
+					<Select
+						className="search-input"
+						placeholder="Search for some car brands to find our awesome products..."
+						options={options}
+					/>
 				</div>
 				<div className="minicart">
 					<FontAwesomeIcon icon="shopping-cart" />
