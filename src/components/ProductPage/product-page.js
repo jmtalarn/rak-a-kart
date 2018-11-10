bfd2d1a567cd5a2930330f7129ed9d4836a565a6
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2018-11-10 01:46:33 +0100

    Responsive product page

--- src/components/ProductPage/product-page.js
+++ src/components/ProductPage/product-page.js
@@ -18,41 +18,56 @@ export default class ProductPage extends Component {
 	renderOptions() {
 		const { product: { info: { options = {} }, selectedOptions = {} } } = this.props;
 
-		return Object.keys(options).map((key, idx) =>
-			(
-				<div key={idx}>
-					{capitalize(key)}
-					{options[ key ].map((item, idx) =>
-						(
-							<Option
-								key={idx}
-								selected={selectedOptions[ key ] === item}
-								onClick={this.selectOption.bind(this, key, item)}
-							>
-								{item}
-							</Option>
-						)
-					)}
-				</div>
-			)
+		return (
+			<div className="product-options">
+				{Object.keys(options).map((key, idx) =>
+					(
+						<div key={idx} >
+							{capitalize(key)}
+							{options[ key ].map((item, idx) =>
+								(
+									<Option
+										key={idx}
+										selected={selectedOptions[ key ] === item}
+										onClick={this.selectOption.bind(this, key, item)}
+									>
+										{item}
+									</Option>
+								)
+							)}
+						</div>
+					)
+				)}
+			</div>
 		);
 
 	}
 	renderProduct() {
 		const { product: { info } } = this.props;
-		console.log(this.props);
+
 		return !isObjectEmpty(info) ?
 			(
 				<article>
-					<img src={info.image} alt={`A visual represetation of the product ${info.name}`} />
-					<h2>{info.name}</h2>
+					<div className="product-image">
+						<img src={info.image} alt={`A visual represetation of the product ${info.name}`} />
+					</div>
+
+					<h2
+						className="product-info-title"
+					>{info.name}</h2>
 					<div className="product-reviews">
 						<Stars value={info.stars} />
 						<Reviews value={info.reviews} />
 					</div>
 					{this.renderOptions()}
-					<p>{info.description}</p>
-					<PriceBox value={info.price} />
+					<p className="product-description">
+						{info.description}
+					</p>
+
+					<div className="product-price">
+						<PriceBox value={info.price} />
+					</div>
+
 				</article>
 			)
 			: null;
